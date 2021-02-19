2a7175286b0e52efb920f91c09ea5bac70c201a0
marbarbosa <74248126+marbarbosa@users.noreply.github.com>
2021-02-19 00:59:03 -0300

    Novo projeto Ping-Query
    Boa noite pessoal.
    Fiz um projeto o ping-query.
    Gostaria de saber é mais ou menos isso...
    Grato,

---
+++ ping-query/PingQuery/Controllers/PingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PingQuery.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class PingController : ControllerBase
+	{
+
+		private readonly ILogger<PingController> _logger;
+
+		public PingController(ILogger<PingController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<Ping> Get()
+		{
+			var rng = new Random();
+			return Enumerable.Range(1, 1).Select(index => new Ping
+			{
+				ID = Guid.NewGuid().ToString(),
+				Post = "Governo diz que a vacinação será por ordem de chegada...",
+				Tags = "#vacina",
+				Imagens = @"C:\Users\Marcos\source\repos\PingQuery\PingQuery\Img\GitHub.png"
+			});
+			}
+	}
+}
---
+++ ping-query/PingQuery/Ping.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PingQuery
+{
+	public class Ping
+	{
+		public string ID { get; set; }
+
+		public string Post { get; set; }
+
+		public string Tags { get; set; }
+
+		public string Imagens { get; set; }
+	}
+}
